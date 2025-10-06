6f8dccc4ab0d7c671358cad1a8c3ed6f3986e7e4
Omkar Padalkar <82218332+omkarpadalkar21@users.noreply.github.com>
2025-10-07 01:35:07 +0530

    Add db config

--- src/main/java/com/omkar/blog/BlogApplication.java
+++ src/main/java/com/omkar/blog/BlogApplication.java
@@ -3,11 +3,19 @@ package com.omkar.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        TimeZone defaultTz = TimeZone.getTimeZone("Asia/Kolkata");
+        TimeZone.setDefault(defaultTz);
+        System.setProperty("user.timezone", "Asia/Kolkata");
+        SpringApplication.run(BlogApplication.class, args);
+
+    }
+
 
 }
