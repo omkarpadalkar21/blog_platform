0d460bc139b17fcc27f9b8a3e5a5dd5c94790ca1
Omkar Padalkar <82218332+omkarpadalkar21@users.noreply.github.com>
2025-10-08 09:10:03 +0530

    Add repositories

--- src/main/java/com/omkar/blog/domain/entities/Post.java
+++ src/main/java/com/omkar/blog/domain/entities/Post.java
@@ -5,7 +5,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -35,12 +37,20 @@ public class Post {
     private Integer readingTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id",nullable = false)
+    @JoinColumn(name = "author_id", nullable = false)
     private User author;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
+
+    @ManyToMany
+    @JoinTable(
+            name = "post_tags",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags = new HashSet<>();
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
---
+++ src/main/java/com/omkar/blog/domain/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.omkar.blog.domain.repositories;
+
+import com.omkar.blog.domain.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+}
