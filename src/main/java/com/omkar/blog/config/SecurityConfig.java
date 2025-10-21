64e11ce1b9bf6dfe8ff372563128acb97a744b69
omkar <omkarpadalkar21@gmail.com>
2025-10-21 12:03:41 +0530

    Complete auth service

--- src/main/java/com/omkar/blog/config/SecurityConfig.java
+++ src/main/java/com/omkar/blog/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.omkar.blog.config;
 
+import com.omkar.blog.domain.entities.User;
+import com.omkar.blog.repositories.UserRepository;
+import com.omkar.blog.security.BlogUserDetailsService;
+import com.omkar.blog.security.JwtAuthFilter;
+import com.omkar.blog.services.AuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,18 +14,42 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public JwtAuthFilter jwtAuthFilter(AuthService authService) {
+        return new JwtAuthFilter(authService);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository repository) {
+        BlogUserDetailsService blogUserDetailsService = new BlogUserDetailsService(repository);
+
+        String email = "omkar@gmail.com";
+        repository.findByEmail(email).orElseGet(() -> {
+            User newUser = User.builder()
+                    .name("Omkar")
+                    .email(email)
+                    .password(passwordEncoder().encode("12345"))
+                    .build();
+
+            return repository.save(newUser);
+        });
+        return blogUserDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         http.
                 authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/tags/**").permitAll()
@@ -29,7 +58,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
--- src/main/java/com/omkar/blog/controllers/AuthController.java
+++ src/main/java/com/omkar/blog/controllers/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/auth/login")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
