6954f8bf135cabf4e1412103710bbc9d015fb7e9
omkar <omkarpadalkar21@gmail.com>
2025-10-24 23:54:17 +0530

    Add endpoint to get draft posts

--- src/main/java/com/omkar/blog/controllers/PostController.java
+++ src/main/java/com/omkar/blog/controllers/PostController.java
@@ -2,8 +2,10 @@ package com.omkar.blog.controllers;
 
 import com.omkar.blog.domain.dtos.PostDto;
 import com.omkar.blog.domain.entities.Post;
+import com.omkar.blog.domain.entities.User;
 import com.omkar.blog.mappers.PostMapper;
 import com.omkar.blog.services.PostService;
+import com.omkar.blog.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,7 @@ public class PostController {
 
     private final PostService postService;
     private final PostMapper postMapper;
+    private final UserService userService;
 
     @GetMapping
     public ResponseEntity<List<PostDto>> getAllPosts(
@@ -27,6 +30,14 @@ public class PostController {
         List<Post> posts = postService.getAllPosts(categoryId, tagId);
         List<PostDto> postDtos = posts.stream().map(postMapper::toDto).toList();
 
+        return ResponseEntity.ok(postDtos);
+    }
+
+    @GetMapping(path = "/drafts")
+    public ResponseEntity<List<PostDto>> getDrafts(@RequestAttribute UUID userId) {
+        User user = userService.getUserById(userId);
+        List<Post> draftPosts = postService.getDraftPosts(user);
+        List<PostDto> postDtos = draftPosts.stream().map(postMapper::toDto).toList();
         return ResponseEntity.ok(postDtos);
     }
 }
